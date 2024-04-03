9645e6b61e9753da30e467508c038baa3360f5e6
babzou <56366864+bastienlandry1@users.noreply.github.com>
2024-04-03 13:13:12 +0200

    Update agrid-install.sh

--- agrid-install.sh
+++ agrid-install.sh
@@ -34,6 +34,7 @@ docker_install(){
     systemctl enable docker
     sudo groupadd docker
     sudo usermod -aG docker $user
+    sudo usermod -aG docker agrid
     sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 }
